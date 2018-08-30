5597e86ce4b274ffe36c6af463d21d8672e08d88
Velitchko Filipov <velitchkofilipov@gmail.com>
2018-08-30 16:40:14 +0200

    view updates

--- src/app/components/view.component/view.component.ts
+++ src/app/components/view.component/view.component.ts
@@ -1,4 +1,4 @@
-import { Inject, Component, AfterViewInit, PLATFORM_ID, ViewChild, ElementRef } from '@angular/core';
+import { Inject, Component, AfterViewInit, PLATFORM_ID, ViewChild, ElementRef, KeyValueChanges, KeyValueDiffer, KeyValueDiffers } from '@angular/core';
 import { isPlatformBrowser, isPlatformServer } from '@angular/common';
 import { Resume } from '../../models/resume';
 import { Skill } from '../../models/skill';
@@ -20,6 +20,7 @@ export class ViewComponent implements AfterViewInit {
   resume: Resume;
   age: number;
   loading: boolean = true;
+  private resumeDiff: KeyValueDiffer<string, any>;
 
   @ViewChild('languages') languages: ElementRef;
   private languageHTML: HTMLElement;
@@ -35,9 +36,11 @@ export class ViewComponent implements AfterViewInit {
     private route: ActivatedRoute,
     @Inject(PLATFORM_ID)
     private platformId: Object,
-    private util: UtilServices
+    private util: UtilServices,
+    private differs: KeyValueDiffers
   ) {
     this.resume = new Resume();
+    this.resumeDiff = this.differs.find(this.resume).create();
     this.route.params.subscribe( (param: Params) => {
       this.selectedId = param['id'];
       this.db.getResume(this.selectedId).then((success: any) => {
@@ -48,175 +51,211 @@ export class ViewComponent implements AfterViewInit {
     });
   }
 
+  ngDoCheck(): void {
+      const changes = this.resumeDiff.diff(this.resume);
+      if (changes) {
+        this.clearSVGs();
+        this.createLanguages(this.resume.languages);
+        this.createSkills(this.resume.skills);
+      }
+  }
+
   ngAfterViewInit(): void {
     if(isPlatformBrowser(this.platformId)) {
       this.languageHTML = this.languages.nativeElement;
       this.createLanguages(this.resume.languages);
+
+      this.skillHTML = this.skills.nativeElement;
       this.createSkills(this.resume.skills);
       // this.createTimeline(this.resume.work, this.resume.education);
     }
   }
+
+  clearSVGs(): void {
+    d3.selectAll('svg').remove();
+  }
+
+
   createTimeline(work: Array<Work>, education: Array<Education>): void {}
 
   createLanguages(languages: Array<Language>): void {
+    console.log('creating languages');
     languages.forEach((l: any, idx: number) => {
       this.createDonut(l, idx);
     })
   }
 
   createSkills(skills: Array<Skill>): void {
-    skills.forEach((s: any, idx: number) => {
-      this.createBar(s, idx);
-    });
+    // OLD VERSION - used barcharts
+    // skills.forEach((s: any, idx: number) => {
+    //   this.createBar(s, idx);
+    // });
+    //
+    this.createSunburst(skills);
   }
 
-  createBar(skill: Skill, id: number): void {
-
+  createBar(skills: Array<Skill>): void {
+    // TODO should go through skills and add
+    // size attribute to leaf nodes (=1)
+    this.createSunburst(skills);
   }
 
-  createSunburst(): void {
-    const width = window.innerWidth,
-          height = window.innerHeight,
-          maxRadius = (Math.min(width, height) / 2) - 5;
+  createSunburst(skills: Array<Skill>): void {
+    // possibly check data structure here
+    // TODO add highlighting
+    // Use opacity or saturation to distinguish expertise
+    let width = 600;
+    let height = 600;
+    let maxRadius = (Math.min(width, height) / 2) - 5;
+    let formatNumber = d3.format(',d');
 
-      const formatNumber = d3.format(',d');
+    let x = d3.scaleLinear()
+                .range([0, 2 * Math.PI])
+                .clamp(true);
 
-      const x = d3.scaleLinear()
-          .range([0, 2 * Math.PI])
-          .clamp(true);
+    let y = d3.scaleSqrt()
+                .range([maxRadius*.1, maxRadius]);
 
-      const y = d3.scaleSqrt()
-          .range([maxRadius*.1, maxRadius]);
+    let color = d3.scaleOrdinal(d3.schemeSet3);
 
-      const color = d3.scaleOrdinal(d3.schemeSet3);
+    let partition = d3.partition();
 
-      const partition = d3.partition();
+    let arc = d3.arc()
+                  .startAngle((d: any) => x(d.x0))
+                  .endAngle((d: any) => x(d.x1))
+                  .innerRadius((d: any) => Math.max(0, y(d.y0)))
+                  .outerRadius((d: any) => Math.max(0, y(d.y1)));
 
-      const arc = d3.arc()
-          .startAngle((d: any) => x(d.x0))
-          .endAngle((d: any) => x(d.x1))
-          .innerRadius((d: any) => Math.max(0, y(d.y0)))
-          .outerRadius((d: any) => Math.max(0, y(d.y1)));
+    let middleArcLine = d => {
+      let halfPi = Math.PI/2;
+      let angles = [x(d.x0) - halfPi, x(d.x1) - halfPi];
+      let r = Math.max(0, (y(d.y0) + y(d.y1)) / 2);
 
-      const middleArcLine = d => {
-          const halfPi = Math.PI/2;
-          const angles = [x(d.x0) - halfPi, x(d.x1) - halfPi];
-          const r = Math.max(0, (y(d.y0) + y(d.y1)) / 2);
+      let middleAngle = (angles[1] + angles[0]) / 2;
+      let invertDirection = middleAngle > 0 && middleAngle < Math.PI; // On lower quadrants write text ccw
+      if (invertDirection) { angles.reverse(); }
 
-          const middleAngle = (angles[1] + angles[0]) / 2;
-          const invertDirection = middleAngle > 0 && middleAngle < Math.PI; // On lower quadrants write text ccw
-          if (invertDirection) { angles.reverse(); }
+      let path = d3.path();
+      path.arc(0, 0, r, angles[0], angles[1], invertDirection);
+      return path.toString();
+    };
 
-          const path = d3.path();
-          path.arc(0, 0, r, angles[0], angles[1], invertDirection);
-          return path.toString();
-      };
+    let textFits = d => {
+      let CHAR_SPACE = 6;
 
-      const textFits = d => {
-          const CHAR_SPACE = 6;
+      let deltaAngle = x(d.x1) - x(d.x0);
+      let r = Math.max(0, (y(d.y0) + y(d.y1)) / 2);
+      let perimeter = r * deltaAngle;
 
-          const deltaAngle = x(d.x1) - x(d.x0);
-          const r = Math.max(0, (y(d.y0) + y(d.y1)) / 2);
-          const perimeter = r * deltaAngle;
+      return d.data.name.length * CHAR_SPACE < perimeter;
+    };
 
-          return d.data.name.length * CHAR_SPACE < perimeter;
-      };
+    let svg = d3.select(this.skillHTML).append('svg')
+                  .style('width', width)
+                  .style('height', height)
+                  .attr('viewBox', `${-width / 2} ${-height / 2} ${width} ${height}`)
+                  .on('click', () => focusOn()); // Reset zoom on canvas click
+    // TODO add profile picture to middle via pattern def
+    // on one of the paths fill="url(#picture)"
+    svg.append("defs")
+         .append('pattern')
+         .attr('id', 'picture')
+         .attr('patternUnits', 'userSpaceOnUse')
+         .attr('width', 300)
+         .attr('height', 300)
+         .append("image")
+         .attr("xlink:href", "./pic.png")
+         .attr('width', 300)
+         .attr('height', 300);
 
-      const svg = d3.select('body').append('svg')
-          .style('width', '100vw')
-          .style('height', '100vh')
-          .attr('viewBox', `${-width / 2} ${-height / 2} ${width} ${height}`)
-          .on('click', () => focusOn()); // Reset zoom on canvas click
+    let root = d3.hierarchy(skills);
+    root.sum((d: any) => d.size);
 
-      d3.json('https://gist.githubusercontent.com/mbostock/4348373/raw/85f18ac90409caa5529b32156aa6e71cf985263f/flare.json', (error, root) => {
-          if (error) throw error;
+    let slice = svg.selectAll('g.slice')
+                     .data(partition(root).descendants());
 
-          root = d3.hierarchy(root);
-          root.sum(d => d.size);
+    slice.exit().remove();
 
-          const slice = svg.selectAll('g.slice')
-              .data(partition(root).descendants());
+    let newSlice = slice.enter()
+                          .append('g').attr('class', 'slice')
+                          .on('click', d => {
+                            d3.event.stopPropagation();
+                            focusOn(d);
+                          });
 
-          slice.exit().remove();
+    newSlice.append('title')
+            .text((d: any) => d.data.name + '\n' + formatNumber(d.value));
 
-          const newSlice = slice.enter()
-              .append('g').attr('class', 'slice')
-              .on('click', d => {
-                  d3.event.stopPropagation();
-                  focusOn(d);
-              });
+    newSlice.append('path')
+            .attr('class', 'main-arc')
+            .style('fill', (d: any) => {
+              return color((d.children ? d : d.parent).data.name);
+            })
+            .attr('d', <any>arc);
 
-          newSlice.append('title')
-              .text((d: any) => d.data.name + '\n' + formatNumber(d.value));
+    newSlice.append('path')
+            .attr('class', 'hidden-arc')
+            .attr('id', (_, i) => `hiddenArc${i}`)
+            .attr('d', middleArcLine);
 
-          newSlice.append('path')
-              .attr('class', 'main-arc')
-              .style('fill', (d: any): any => color((d.children ? d : d.parent).data.name))
-              .attr('d', <any>arc);
+    let text = newSlice.append('text')
+                         .attr('class', 'label')
+                         .attr('display', d => textFits(d) ? null : 'none');
 
-          newSlice.append('path')
-              .attr('class', 'hidden-arc')
-              .attr('id', (_, i) => `hiddenArc${i}`)
-              .attr('d', middleArcLine);
+    // Add white contour
+    text.append('textPath')
+        .attr('startOffset','50%')
+        .attr('xlink:href', (_, i) => `#hiddenArc${i}` )
+        .text((d: any) => d.data.name)
+        .style('fill', 'none')
+        .style('stroke', '#fff')
+        .style('stroke-width', 3)
+        .style('stroke-linejoin', 'round');
 
-          const text = newSlice.append('text')
-              .attr('display', d => textFits(d) ? null : 'none');
-
-          // Add white contour
-          text.append('textPath')
-              .attr('startOffset','50%')
-              .attr('xlink:href', (_, i) => `#hiddenArc${i}` )
-              .text((d: any) => d.data.name)
-              .style('fill', 'none')
-              .style('stroke', '#fff')
-              .style('stroke-width', 5)
-              .style('stroke-linejoin', 'round');
-
-          text.append('textPath')
-              .attr('startOffset','50%')
-              .attr('xlink:href', (_, i) => `#hiddenArc${i}` )
-              .text((d: any) => d.data.name);
-      });
-
-      function focusOn(d = { x0: 0, x1: 1, y0: 0, y1: 1 }) {
-          // Reset to top-level if no data point specified
-
-          const transition = svg.transition()
-              .duration(750)
-              .tween('scale', () => {
-                  const xd = d3.interpolate(x.domain(), [d.x0, d.x1]),
-                      yd = d3.interpolate(y.domain(), [d.y0, 1]);
-                  return t => { x.domain(xd(t)); y.domain(yd(t)); };
-              });
-
-          transition.selectAll('path.main-arc')
-              .attrTween('d', (d: any) => () => arc(d));
-
-          transition.selectAll('path.hidden-arc')
-              .attrTween('d', d => () => middleArcLine(d));
-
-          transition.selectAll('text')
-              .attrTween('display', d => () => textFits(d) ? null : 'none');
-
-          this.moveStackToFront(svg, d);
-
-          //
+    text.append('textPath')
+        .attr('startOffset','50%')
+        .attr('xlink:href', (_, i) => `#hiddenArc${i}` )
+        .text((d: any) => d.data.name);
 
 
+
+    function focusOn(d = { x0: 0, x1: 1, y0: 0, y1: 1 }) {
+      // Reset to top-level if no data point specified
+      let transition = svg.transition()
+                            .duration(750)
+                            .tween('scale', () => {
+                              let xd = d3.interpolate(x.domain(), [d.x0, d.x1]),
+                              yd = d3.interpolate(y.domain(), [d.y0, 1]);
+                              return t => { x.domain(xd(t)); y.domain(yd(t)); };
+                            });
+
+      transition.selectAll('path.main-arc')
+                .attrTween('d', (d: any) => () => arc(d));
+
+      transition.selectAll('path.hidden-arc')
+                .attrTween('d', d => () => middleArcLine(d));
+
+      transition.selectAll('text')
+                .attrTween('display', d => () => textFits(d) ? null : 'none');
+
+      moveStackToFront(d);
+
+      function moveStackToFront(elD) {
+        svg.selectAll('.slice')
+           .filter(d => d === elD)
+           .each(function(d: any) {
+             // cast to HTML element type so typescript doenst complain
+             (<HTMLElement>this).parentNode.appendChild(<HTMLElement>this);
+             if (d.parent) { moveStackToFront(d.parent); }
+           });
       }
-  }
-
-  moveStackToFront(svg, elD): void {
-   svg.selectAll('.slice').filter(d => d === elD)
-      .each(function(d) {
-        this.parentNode.appendChild(this);
-        if (d.parent) { this.moveStackToFront(svg, d.parent); }
-      });
+    }
   }
 
   createDonut(language: Language, id: number): void {
+    console.log(language.name + ' ' + language.level);
     // maps languages proficiency to donut fill
-    const skillLevel = {
+    let skillLevel = {
 			'A1' : 16.6666666667,
 			'A2' : 33.3333333334,
 			'B1' : 50.0000000001,
@@ -226,22 +265,22 @@ export class ViewComponent implements AfterViewInit {
 		};
 
     // setup data in form of { language, proficiency }
-    const dataset = [
+    let dataset = [
       skillLevel[language.level],       // colored area
       100 - skillLevel[language.level]  // transparent area
     ];
 
     // donut dimensions
-    const width = 250;
-    const height = 250;
-    const donutWidth = 75;
-    const dataSize = 25;
-    const duration = 750;
-    const radius = Math.min(width, height) / 2;
+    let width = 250;
+    let height = 250;
+    let donutWidth = 75;
+    let dataSize = 25;
+    let duration = 750;
+    let radius = Math.min(width, height) / 2;
     // color scheme
-    const color = ['#00d1b2', '#c5f9f2'];
+    let color = ['#00d1b2', '#c5f9f2'];
     // create SVG and append
-    const svg = d3.select(this.languageHTML)
+    let svg = d3.select(this.languageHTML)
             .append('svg')
             .attr('width', width)
             .attr('height', height)
@@ -249,14 +288,14 @@ export class ViewComponent implements AfterViewInit {
             .attr('class', `g-${id}`)
             .attr('transform', `translate(${width / 2}, ${height / 2})`);
     // load data into pie and append
-    const arc = d3.arc()
+    let arc = d3.arc()
              .outerRadius(radius - donutWidth + dataSize)
              .innerRadius(radius - donutWidth);
     // create angles
-    const pie = d3.pie()
+    let pie = d3.pie()
              .value((d: any) => d);
     // draw svg
-    const path = svg.selectAll('path')
+    let path = svg.selectAll('path')
              .data(pie(dataset))
              .enter().append('path')
              .attr('d', <any>arc)
@@ -282,7 +321,7 @@ export class ViewComponent implements AfterViewInit {
                 }
             });
     // append text labels language name + proficiency
-    const g = d3.select(`.g-${id}`)
+    let g = d3.select(`.g-${id}`)
 
     g.append('text')
      .attr('text-anchor', 'middle')
@@ -298,6 +337,6 @@ export class ViewComponent implements AfterViewInit {
      .attr('font-size', '2.5em')
      .attr('y', 35)
      .text(language.level);
-    // http://zeroviscosity.com/d3-js-step-by-step/step-1-a-basic-pie-chart
   }
+
 }
