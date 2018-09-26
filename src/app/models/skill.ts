560d02dbf98e1de373a5df837e74a9cfc10e87eb
Velitchko Filipov <velitchkofilipov@gmail.com>
2018-09-26 15:21:40 +0200

    resume skill model updated

--- src/app/models/skill.ts
+++ src/app/models/skill.ts
@@ -1,10 +1,14 @@
 export class Skill {
   name: string;
   level: string;
+  experience: number;
   size?: number; // optional only set at leaf nodes
   children: Array<Skill>; // nested skills
 
-  constructor() {
+  constructor(name?: string, level?: string, experience?: number) {
+    this.name = name || '';
+    this.level = level || '';
+    this.experience = experience || 0;
     this.children = new Array<Skill>();
   }
 }
