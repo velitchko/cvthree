4803a9f4f0919cd6b6bc027cc1127119a4cdc4f8
Velitchko Filipov <velitchkofilipov@gmail.com>
2018-11-11 15:56:10 +0100

    util added functions

--- src/app/services/util.service.ts
+++ src/app/services/util.service.ts
@@ -5,12 +5,16 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { Resume } from '../models/resume';
 import { DomSanitizer, SafeStyle } from '@angular/platform-browser';
-
+import * as moment from 'moment';
 @Injectable()
 
 export class UtilServices {
 
   constructor(private sanitization: DomSanitizer) {}
+
+  getInitials(firstName: string, lastName: string): string {
+    return `${firstName.charAt(0)}${lastName.charAt(0)}`;
+  }
 
   getAge(birthDay: Date): number {
     let ageDifMs = Date.now() - birthDay.getTime();
@@ -21,4 +25,11 @@ export class UtilServices {
   getSanitizedPicture(picture: string): SafeStyle {
     return this.sanitization.bypassSecurityTrustStyle(`url(${picture})`);
   }
+
+  getDateDifference(a: Date, b: Date): number {
+    let start = moment(a, 'DD/MM/YYYY');
+    let end = b ? moment(b, 'DD/MM/YYYY') : moment(Date.now(), 'DD/MM/YYYY');
+    return end.diff(b, 'years');
+  }
+
 }
