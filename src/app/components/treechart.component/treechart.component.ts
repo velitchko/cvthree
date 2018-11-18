14ab78a9aae4d732b6fd5edcedb7a31b47f1906f
Velitchko Filipov <velitchkofilipov@gmail.com>
2018-11-18 16:26:50 +0100

    treechart radial layout

--- src/app/components/treechart.component/treechart.component.ts
+++ src/app/components/treechart.component/treechart.component.ts
@@ -77,7 +77,7 @@ export class TreeChartComponent implements OnChanges {
             .attr('stroke-opacity', () => {
               return this.getSkillOpacity(d.data.level);
             })
-            .attr('stroke-width', 10)
+            .attr('stroke-width', 8)
             .attr('stroke', () => {
               return this.cs.getColorForResume(resumeID);
             });
@@ -88,270 +88,109 @@ export class TreeChartComponent implements OnChanges {
     let links = root.links();
     links.forEach((l: any) => {
       d3.selectAll('path.link').each((dl: any, il: any, nl: any) => {
-        if (!dl.data.people.includes(resumeID)) return;
-        if (l.source.data.name === dl.parent.data.name && l.target.data.name === dl.data.name) {
+        if (!dl.target.data.people.includes(resumeID)) return;
+       // if (l.source.data.name === dl.target.parent.data.name && dl.source.data.name === l.target.parent.data.name) {
           d3.select(nl[il])
             .transition()
             .attr('stroke-width', 4)
             .attr('stroke', () => {
               return this.cs.getColorForResume(resumeID);
             });
-        }
+       //  }
       });
     })
   }
 
-  drawTree(id: string, treeData: any): void {
+  drawTree(id: string, data: any): void {
     // First check this out : https://medium.com/@c_behrens/enter-update-exit-6cafc6014c36 to understand how enter, update, exit works
     // Tree already in proper format
+    d3.select(id).select('svg').remove(); 
+    // set the dimensions and margins of the diagram
+    let margin = { top: 20, right: 30, bottom: 30, left: 30 },
+      width = 700, // - margin.left - margin.right,
+      height = 600; // - margin.top - margin.bottom;
+    let nodeRadius = 25;
+    let nodeThickness = 10;
+    let duration = 250;
 
-    // Setup SVG Element - Start
-    d3.select(id).select('svg').remove();  // from previous
-    let margin = { top: 30, right: 20, bottom: 30, left: 20 },
-      width = 960 - margin.left - margin.right,
-      height = 500 - margin.top - margin.bottom;
-
-    let circleSize = 25;
-
-    let svg = d3.select(id)
-      .append('svg')
-      .attr('width', width + margin.right + margin.left)
-      .attr('height', height + margin.top + margin.bottom)
-
-    let g = svg.append('g')
-      .attr('transform', 'translate(' + width/2 + ',' + height/2 + ')');
-
-    // Setup SVG Element - End
-
-    let i = 0,
-      duration = 750,
-      root;
-
-    // Setup tree
-
+    // declares a tree layout and assigns the size
     let treemap = d3.tree()
-      .size([width, height])
-      .nodeSize([circleSize*2 + 5, circleSize*2])
-      .separation((a: any, b: any) => { return (a.parent === b.parent ? 1 : 2); });
-
-    // Get the root
-
-    root = d3.hierarchy(treeData, function (d: any) { return d.children; });
-
-    root.x0 = 0;
-    root.y0 = width / 3;
-
-    // Collapse all children, except root's
-    // root.children.forEach(collapse);
-    // root.children = null;
-
-    // Let's draw the tree
-    draw(root, this);
-
-    function draw(source, _self) {
-
-      // Get the treemap, so that we can get nodes and links
-      let treeData = treemap(root);
-
-      // Get nodes and links
-      let nodes = treeData.descendants(),
-        links = treeData.descendants().slice(1);
-
-      // Adjust the position of y of each node. Comment out just this line and see how it's different  
-      nodes.forEach(function (d: any) { d.y = d.depth * 100 });
-
-      // Add unique id for each node, else it won't work
-      let node = g.selectAll('g.node')
-        .data(nodes, function (d: any) { return d.id || (d.id = ++i); });
-      let thickness = 7; // in pixels
-
-      // Let's append all enter nodes
-      let nodeEnter = node
-        .enter()
-        .append('g')
-        .attr('class', 'node')
-        .attr('transform', function () {
-          return 'translate(' + source.x0 + ',' + source.y0 + ')';
-        })
-        .on('click', (d: any) => {
-          // FIXME: bug with collapsing nodes - first one doesnt get rerendered
-          // and links get jumbled up
-          // if (d.children) {
-          //   d._children = d.children;
-          //   d.children = null;
-          // } else {
-          //   d.children = d._children;
-          //   d._children = null;
-          // }
-          // let children = d._children ? d._children : d.children;
-          // children.forEach((function (child) {
-          //   collapse(child);
-          // }));
-          // // If d has a parent, collapse other children of that parent
-          // if (d.parent) {
-          //   d.parent.children.forEach(function (element) {
-          //     if (d !== element) {
-          //       collapse(element);
-          //     }
-          //   });
-          // }
-          draw(d, _self);
-        });
-      // create nodes as donut charts
-      _self.getNode(nodeEnter, circleSize, thickness, nodes);
-      // Points equally spread along the inside of the circle for each person
-      // nodeEnter.each((d: any, i: any, n: any) => {
-      //   if (d.data.people.length === 0) return;
-      //   let smallRadius = 5;
-      //   let radius = circleSize - smallRadius;
-      //   let angle = (360 / d.data.people.length) * (Math.PI / 180);
-      //   let nodeContainer = d3.select(n[i]);
-      //   // console.log(radius, angle, d.data.name, d.data.people.length)
-      //   d.data.people.forEach((pers: any, idx: number) => {
-      //     // console.log(radius, angle, idx, Math.cos(angle*idx), d.x0, d.y0)
-      //     nodeContainer.append('circle')
-      //       .attr('r', smallRadius)
-      //       .attr('fill', _self.cs.getColorForResume(pers))
-      //       .attr('cx', () => {
-      //         return radius * Math.cos(angle * idx);
-      //       })
-      //       .attr('cy', () => {
-      //         return radius * Math.sin(angle * idx);
-      //       })
-      //   });
-      // });
-
-      // Add text
-      nodeEnter.append('text')
-        .text(function (d: any) { return d.data.name; })
-        .attr('y', (d: any, i: any, n: any) => {
-          let bbox = d3.select(n[i]).node().getBBox();
-          return circleSize + bbox.height + 5; // 10 is the stroke we have on highlighting 
-        })
-        .attr('x', function (d: any, i: any, n: any) {
-          let bbox = d3.select(n[i]).node().getBBox();
-          return -1 * bbox.width/2;
-        })
-        // .attr('text-anchor', function (d: any) {
-        //   return d.children || d._children ? 'end' : 'start';
-        // });
-
-      // https://github.com/d3/d3-selection/issues/86 to check what merge does
-      let nodeUpdate = nodeEnter.merge(node);
-
-      // Do transition of node to appropriate position
-      nodeUpdate.transition()
-        .duration(duration)
-        .attr('transform', function (d) {
-          return 'translate(' + d.x + ',' + d.y + ')';
-        });
+      .size([360, 250]) //angle = 2*PI, radius = 250
+      .nodeSize([nodeRadius*4, nodeRadius*4])
+      .separation(function (a, b) { return (a.parent == b.parent ? 1 : 2); });
 
 
-      // Let's update the radius now, which was previously zero.
-      nodeUpdate.select('circle.node')
-        // .attr('r', circleSize)
-        // .attr('stroke-width', 2)
-        // .attr('stroke', '#D4D8DA')
-        // .attr('fill', function (d: any) {
-        //   return d._children ? 'lightsteelblue' : '#fff';
-        // })
-        .attr('cursor', 'pointer')
-      
-
-
-      // Let's work on exiting nodes
-      // Remove the node
-      let nodeExit = node.exit().transition()
-        .duration(duration)
-        .attr('transform', function (d) {
-          return 'translate(' + source.x + ',' + source.y + ')';
-        })
-        .remove();
-
-      // On exit reduce the node circles size to 0
-      nodeExit.select('circle')
-        .attr('r', 0);
-
-      // On exit reduce the opacity of text labels
-      nodeExit.select('text')
-        .attr('fill-opacity', 0);
-
-      // Let's draw links
-      let link = g.selectAll('path.link')
-        .data(links, (d: any) => { return d.id; });
-
-      // Work on enter links, draw straight lines
-      let linkEnter = link.enter()
-        .insert('path', 'g')
-        .attr('class', 'link')
-        .attr('stroke-width', 2)
-        .attr('source', (d: any) => {
-          return d.parent.data.name;
-        })
-        .attr('target', (d: any) => {
-          return d.data.name;
-        })
-        .attr('stroke', '#D4D8DA')
-        .attr('d', function() {
-          let o = { x: source.x0, y: source.y0 }
-          return diagonal(o, o)
-        });
-
-      // UPDATE
-      let linkUpdate = linkEnter.merge(link);
-
-      // Transition back to the parent element position, now draw a link from node to it's parent
-      linkUpdate.transition()
-        .duration(duration)
-        .attr('d', (d: any) => { return diagonal(d, d.parent) });
-
-      // Remove any exiting links
-      let linkExit = link.exit().transition()
-        .duration(duration)
-        .attr('d', function() {
-          let o = { x: source.x, y: source.y }
-          return diagonal(o, o)
-        })
-        .remove();
-
-      // Store the old positions for transition.
-      nodes.forEach(function (d: any) {
-        d.x0 = d.x;
-        d.y0 = d.y;
-      });
-
+    function radialPoint(x, y) {
+      return [y * Math.cos(x -= Math.PI / 2), y * Math.sin(x)];
     }
 
-    function diagonal(s, d) {
+    let root;
 
-      // Here we are just drawing lines, we can also draw curves, comment out below path for it.
+    let nodes = d3.hierarchy(data, function (d: any) {
+      return d.children;
+    });
 
-      let path = `M ${s.x} ${s.y}
-          L ${d.x} ${d.y}`;
+    root = treemap(nodes);
+    let nodeData = root.descendants();
+    let linkData = root.descendants().slice(1);
+    let svg = d3.select(id).append('svg')
+      .attr('width', width + margin.left + margin.right)
+      .attr('height', height + margin.top + margin.bottom)
+      .attr('class', 'chart'),
+      g = svg.append('g')
+        .attr('transform',
+          'translate(' + (width / 2) + ',' + (height / 2) + ')'); //make sure the center of circle is in the center of svg
 
-      // let path = `M ${s.x} ${s.y}
-      //         C ${(s.x + d.x) / 2} ${s.y},
-      //           ${(s.x + d.x) / 2} ${d.y},
-      //           ${d.x} ${d.y}`
+    // adds the links between the nodes
+    let links = g.selectAll('.link')
+      .data(root.links());
 
-      return path;
-    }
+    let linkEnter = links.enter().append('path')
+      .attr('class', 'link')
+      .attr('d', d3.linkRadial().angle((d: any) => { return 0; }).radius((d: any) => { return 0; }))
+      .attr('stroke', function (d, i) { return '#D4D8DA'; })
+      .attr('stroke-width', 2)
+      .attr('stroke-opacity', 0.75);
 
-    function collapse(d) {
-      // console.log(d);
-      if (d.children) {
-        d._children = d.children
-        d._children.forEach(collapse)
-        d.children = null
-      }
-    }
+    let linkUpdate = links.merge(linkEnter)
+      .transition()
+      .duration(duration)
+      .attr('d', d3.linkRadial()
+        .angle(function (d: any) { return d.x; })
+        .radius(function (d: any) { return d.y; }));
+
+    // adds each node as a group
+    let node = g.selectAll('.node')
+      .data(nodeData);
+
+    //arrange each node on svg
+    let nodeEnter = node.enter().append('g').attr('class', 'node');
+  
+    // adds the circle to the node  
+    this.getNode(nodeEnter, nodeRadius - 2, nodeThickness - 2, nodeData);
+
+    // adds the text to the node
+    nodeEnter.append('text')
+      .attr('class', 'all')
+      .text(function (d: any) { return d.data.name; })
+      .attr('dy', '.35em')
+      .attr('x', function (d: any) { 
+        if(d.data.name === 'Skills') return -12;
+        return d.x < Math.PI ? 35 : -35; 
+      })
+      .style('text-anchor', function (d: any) { return d.x < Math.PI ? 'start' : 'end'; })
+      .attr('transform', function (d: any) { 
+        if(d.data.name === 'Skills') return;
+        return 'rotate(' + (d.x < Math.PI ? d.x - Math.PI / 2 : d.x + Math.PI / 2) * 180 / Math.PI + ')'; 
+      })
+
+    let nodeUpdate = node.merge(nodeEnter)
+      .transition()
+      .duration(duration)
+      .attr('transform', function (d: any) { return 'translate(' + radialPoint(d.x, d.y) + ')'; });
   }
 
   getNode(node: any, circleSize: number, thickness: number, nodeData: any): void {
-    // console.log('generating node');
-    // console.log(data.name, data.people);
-    // console.log(data); 
-    // return;
     let donutData = new Array<any>();
     nodeData.forEach((n: any) => {
       let nodeArr = new Array<any>();
@@ -372,19 +211,12 @@ export class TreeChartComponent implements OnChanges {
       .attr('stroke-width', (d: any) => {
         return d.data.name === 'Skills' ? 6 : 0;
       });
-  
-    // node.append("circle") //background circle fill
-    //   .attr("cx", 0)
-    //   .attr("cy", 0)
-    //   .attr("r", circleSize - thickness)
-    //   .attr("fill", "#F1F1F1");
 
     let arc = d3.arc()
       .innerRadius(circleSize - thickness)
       .outerRadius(circleSize);
     let pie = d3.pie().value((d: any) => { return d.skills[1]; });
 
-    // let values = donutData.map(m => { return m[1]; });
     node.selectAll('path')
       .data((d: any, i: any) => {
         return pie(donutData[i]);
@@ -394,34 +226,12 @@ export class TreeChartComponent implements OnChanges {
       .append('path')
       .attr('d', <any>arc)
       .attr('fill', (d: any) => { return this.cs.getColorForResume(d.data.skills[0]); })
-      // .attr('fill-opacity', (d: any) => { 
-      //   this wasn't such a good idea
-      //   let skill = this.cs.searchForSkill(d.data.skills[0], d.data.name);
-      //   return this.getSkillOpacity(skill ? skill.level : '');
-      // //  return this.getSkillOpacity(); 
-      // })
       .on('mouseover', (d: any, i: any, n: any) => {
         if (d.data.skills[0]) this.selectedResume.emit(d.data.skills[0]);
-        // d3.select(n[i])
-        //   .attr('stroke-width', 4)
-        //   .attr('stroke', this.cs.getColorForResume(d.data[0]));
       })
       .on('mouseout', (d: any, i: any, n: any) => {
         this.selectedResume.emit('none');
-        // d3.select(n[i])
-        // .attr('stroke-width', 4)
-        // .attr('stroke', 'transparent');
       });
-      // .transition()
-      // .delay((d, i) => { return i * 25; })
-      // .duration(150)
-      // .attrTween('d', (d: any) => {
-      //   var i = d3.interpolate(d.startAngle + 0.1, d.endAngle);
-      //   return function (t: any) {
-      //     d.endAngle = i(t);
-      //     return arc(d);
-      //   }
-      // });
   }
 
 }
